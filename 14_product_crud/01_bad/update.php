6527b42a1093341722d12b1a3660e04e18df21f9
Christian Boyle <mail@christianboyle.com>
2021-01-03 17:33:08 -0600

    Add exercise 14, use Bootstrap v5.0.0-beta1 instead of v4.5.2

--- 14_product_crud/01_bad/update.php
+++ 14_product_crud/01_bad/update.php
@@ -65,59 +65,60 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $statement->execute();
         header('Location: index.php');
     }
-
 }
 
 ?>
 <!doctype html>
 <html lang="en">
+
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
-          integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-    <link href="app.css" rel="stylesheet"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link href="app.css" rel="stylesheet" />
     <title>Products CRUD</title>
 </head>
+
 <body>
-<p>
-    <a href="index.php" class="btn btn-default">Back to products</a>
-</p>
-<h1>Update Product: <b><?php echo $product['title'] ?></b></h1>
+    <p>
+        <a href="index.php" class="btn btn-default">Back to products</a>
+    </p>
+    <h1>Update Product: <b><?php echo $product['title'] ?></b></h1>
 
-<?php if (!empty($errors)): ?>
-    <div class="alert alert-danger">
-        <?php foreach ($errors as $error): ?>
-            <div><?php echo $error ?></div>
-        <?php endforeach; ?>
-    </div>
-<?php endif; ?>
-
-<form method="post" enctype="multipart/form-data">
-    <?php if ($product['image']): ?>
-        <img src="<?php echo $product['image'] ?>" class="product-img-view">
+    <?php if (!empty($errors)) : ?>
+        <div class="alert alert-danger">
+            <?php foreach ($errors as $error) : ?>
+                <div><?php echo $error ?></div>
+            <?php endforeach; ?>
+        </div>
     <?php endif; ?>
-    <div class="form-group">
-        <label>Product Image</label><br>
-        <input type="file" name="image">
-    </div>
-    <div class="form-group">
-        <label>Product title</label>
-        <input type="text" name="title" class="form-control" value="<?php echo $title ?>">
-    </div>
-    <div class="form-group">
-        <label>Product description</label>
-        <textarea class="form-control" name="description"><?php echo $description ?></textarea>
-    </div>
-    <div class="form-group">
-        <label>Product price</label>
-        <input type="number" step=".01" name="price" class="form-control" value="<?php echo $price ?>">
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+
+    <form method="post" enctype="multipart/form-data">
+        <?php if ($product['image']) : ?>
+            <img src="<?php echo $product['image'] ?>" class="product-img-view">
+        <?php endif; ?>
+        <div class="form-group">
+            <label>Product Image</label><br>
+            <input type="file" name="image">
+        </div>
+        <div class="form-group">
+            <label>Product title</label>
+            <input type="text" name="title" class="form-control" value="<?php echo $title ?>">
+        </div>
+        <div class="form-group">
+            <label>Product description</label>
+            <textarea class="form-control" name="description"><?php echo $description ?></textarea>
+        </div>
+        <div class="form-group">
+            <label>Product price</label>
+            <input type="number" step=".01" name="price" class="form-control" value="<?php echo $price ?>">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
 
 </body>
+
 </html>
